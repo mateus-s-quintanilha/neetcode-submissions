f91f163ad21d81db251834e51aff34ad3811e433
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-06-27 15:26:32 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const map = new Object();
+        const sArr = s.split('')
+        const tArr = t.split('')
+        
+        for(const numS of sArr) {
+            if(map[numS]) {
+                map[numS] += 1
+            } else {
+                map[numS] = 1
+            }
+        }
+
+        for(const numT of tArr) {
+            if(map[numT]) {
+                map[numT] -= 1;
+                if(map[numT] <= 0) delete map[numT]; 
+            } else {
+                return false;
+            }
+        }
+
+        return Object.keys(map).length === 0;
+    }
+}
