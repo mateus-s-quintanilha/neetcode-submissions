88d2c944e7ab2e3a23711244a772045b4ac1c840
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-07-01 18:06:15 +0100

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if(nums.length <= 1) return nums.length
+        let result = 1
+
+        for(let i = 0; i < nums.length; i++) {
+            const currSeq = [nums[i]];
+
+            let j = 0;
+
+            while(j < nums.length) {
+                const curr = nums[j];
+                if(curr === currSeq[currSeq.length-1]+1) {
+                    currSeq.push(curr);
+                    j = 0;
+                } else {
+                    j++
+                }
+            };
+
+            result = Math.max(result, currSeq.length)
+        };
+
+        return result;
+    }
+}
