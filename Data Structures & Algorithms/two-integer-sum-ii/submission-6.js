431a671bdc165b4e8af1d65c4d3ad0477e65161f
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-07-03 17:36:05 +0100

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        if(numbers.length === 2) return numbers.map((_, i) => i+1);
+        
+        // const targetIdx = numbers.length >= target-1 ? (target-1) -1 : numbers.length - 1;
+        const targetIdx = numbers.length - 1;
+
+        let p1 = 0;
+        let p2 = targetIdx
+
+        while(p1 < p2) {
+            let n1 = numbers[p1];
+            let n2 = numbers[p2];
+
+            const sum = n1 + n2
+
+            console.log({sum, n1, n2, p1, p2})
+
+            if(sum === target) {
+                return [p1+1, p2+1];
+            }
+
+            if(sum > target) {
+                p2--;
+                continue;
+            }
+
+            if(sum < target) {
+                p1++
+                continue;
+            }
+        }
+    }
+}
