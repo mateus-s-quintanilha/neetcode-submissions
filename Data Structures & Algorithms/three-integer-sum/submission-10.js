88c8aefaf1febca674e1e28969bf1cff8f1b371d
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-07-04 02:29:19 +0100

    Add: three-integer-sum - submission-10

---
+++ Data Structures & Algorithms/three-integer-sum/submission-10.js
@@ -0,0 +1,71 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    _threeSum(nums) {
+        const res = []
+        const computedRes = {}
+
+        for(let i = 0; i < nums.length; i++) {
+            for(let j = 1; j < nums.length; j++) {
+                if(i === j) continue
+
+                for(let k = 2; k < nums.length; k++) {
+                    if(k === j || k === i) continue
+                    const n1 = nums[i];
+                    const n2 = nums[j];
+                    const n3 = nums[k];
+
+
+                    if(n1 + n2 + n3 === 0) {
+                        const joinRes = [n1, n2, n3].sort().join(',')
+                        if(!computedRes[joinRes]) {
+                            res.push([n1, n2, n3])
+                            computedRes[joinRes] = true;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    threeSum(nums) {
+        const numsSorted = [...nums].sort((a, b) => a-b);
+        const result = new Array();
+
+        for(let i = 0; i < numsSorted.length-2; i++) {
+            const base = numsSorted[i]
+            // if(base >= 1 || (i > 0 && numsSorted[i] === numsSorted[i-1])) return result
+            if(i > 0 && numsSorted[i] === numsSorted[i-1]) continue
+
+            let pL = i+1
+            let pR = numsSorted.length-1
+
+            while(pL < pR) {
+                const sum = base + numsSorted[pL] + numsSorted[pR]
+                if(sum > 0) {
+                    pR--
+                } else if(sum === 0) {
+                    result.push([base, numsSorted[pL], numsSorted[pR]])
+                    pL++
+                    while(pL < pR && numsSorted[pL] === numsSorted[pL-1]) {
+                        pL++
+                    }
+                } else {
+                    pL++
+                }
+            }
+        }
+
+        return result;
+    };
+
+}
+
+[-4,-1,-1,0,1,2]
+
+[[-1,-1,2],[-1,0,1]]
