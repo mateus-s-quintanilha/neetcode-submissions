c099d5a081b0ee7e1bddd8f0c52055614b2749b3
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-07-05 22:02:53 +0100

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let waterQty = 0;
+
+        for(let i = 1; i < height.length-1; i++) {
+            let biggestHeightLeft = 0
+            let l = i-1
+            while(l >= 0) {
+                biggestHeightLeft = Math.max(biggestHeightLeft, height[l]);
+                l--;
+            };
+
+            let biggestHeightRight = 0
+            let r = i+1;
+            while(r < height.length) {
+                biggestHeightRight = Math.max(biggestHeightRight, height[r]);
+                r++;
+            };
+
+            const currTrappedWater = Math.min(biggestHeightRight, biggestHeightLeft) - height[i];
+            if(currTrappedWater > 0) waterQty += currTrappedWater;
+        }
+
+        return waterQty
+    };
+};
+//  i
+// [0,1,0,2,1,0,1,3,2,1,2,1]
+
