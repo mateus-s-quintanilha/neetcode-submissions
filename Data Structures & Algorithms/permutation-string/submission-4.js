4670d5d36b754bb648c43ece84dffeefa71edade
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-07-10 23:21:31 +0100

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        if(s2.length < s1.length) return false;
+
+        const mappedS1 = {};
+        for(let i = 0; i < s1.length; i++) {
+            const char = s1[i];
+
+            mappedS1[char] = (mappedS1[char] ?? 0) + 1 
+        }
+
+        let copyMappedS1 = JSON.parse(JSON.stringify(mappedS1));
+
+        let l = 0;
+        let r = l;
+        while(r < s2.length) {
+            const char = s2[r];
+
+            if(char in copyMappedS1) {
+                copyMappedS1[char] = copyMappedS1[char] - 1
+                if(copyMappedS1[char] === 0) delete copyMappedS1[char]
+
+                if(!Object.keys(copyMappedS1).length) return true
+
+                r++
+            } else {
+                l++
+                r = l;
+                copyMappedS1 = JSON.parse(JSON.stringify(mappedS1));
+            }
+        }
+
+        return false;
+    }
+}
+
+
+// { a: 1}
+// l r
+// dcda
