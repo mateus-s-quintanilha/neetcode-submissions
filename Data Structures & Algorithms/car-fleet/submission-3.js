b65f73de04f6221a2a4f6263be1b1b05fab08455
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-07-19 23:41:45 +0100

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        if (target.length === 1) return 1;
+
+        let positionMap = {};
+
+        for (let i = 0; i < position.length; i++) {
+            positionMap[position[i]] = speed[i];
+        }
+
+        position.sort((a, b) => a - b);
+
+        const stack = [];
+
+        stack.push(
+            (target - position[position.length - 1]) / positionMap[position[position.length - 1]],
+        );
+
+        for (let i = position.length - 2; i >= 0; i--) {
+            const speed = positionMap[position[i]];
+            const timeToArrive = (target - position[i]) / speed;
+
+            const isGonnaFleet = timeToArrive <= stack[stack.length - 1];
+
+            if (!isGonnaFleet) {
+                stack.push(timeToArrive);
+            }
+        }
+
+        return stack.length;
+    }
+}
+
