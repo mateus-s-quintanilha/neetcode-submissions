f524e445f3c296d30b3f254884e3eaa6d385a5cb
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-08-03 10:54:59 +0100

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        for(let i = 0; i < matrix.length; i++) {
+            const row = matrix[i];
+            if(target < row[0]) break;
+            
+            const couldBeBetween = target >= row[0] && target <= row[row.length-1];
+
+            if(couldBeBetween) {
+                let l = 0
+                let r = row.length -1
+
+                while(l <= r) {
+                    const m = Math.floor((r + l) / 2);
+
+                    if(row[m] < target) {
+                        l = m+1;
+                    } else if(row[m] > target) {
+                        r = m-1;
+                    } else {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
+
+[0, 1, 2, 3, 4, 5, 6]
