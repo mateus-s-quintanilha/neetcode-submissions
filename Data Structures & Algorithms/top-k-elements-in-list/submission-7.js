8433e00ac90bbe3033cc943c4314e0b0e3b73c22
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-08-10 23:44:28 +0100

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+
+    // bucket list algorithm
+    topKFrequent(nums, k) {
+        const bucketList = Array.from({ length: nums.length+1 }, () => []);
+
+        const numsFreq = {};
+        for(let num of nums) {
+            numsFreq[num] = (numsFreq[num] ?? 0) + 1;
+        }
+
+        for(let key in numsFreq) {
+            const freq = Number(numsFreq[key]);
+            bucketList[freq].push(Number(key))
+        };
+
+        const res = [];
+        for(let i = bucketList.length -1; i >= 0; i--) {
+            if(!bucketList[i].length) continue;
+            
+            for(let n of bucketList[i]) {
+                res.push(n);
+                if(res.length === k) return res;
+            }
+        }
+        return res;
+    }
+}
