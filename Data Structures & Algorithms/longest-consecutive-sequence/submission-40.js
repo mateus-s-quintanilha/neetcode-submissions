3d708a68bc57157411579b962c838d4af75afe39
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-08-12 16:17:24 +0100

    Add: longest-consecutive-sequence - submission-40

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-40.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if(nums.length <= 1) return nums.length;
+
+        let maxSeq = 1;
+        const seenMap = new Set(nums);
+        for(const num of nums) {
+            const isStart = !seenMap.has(num-1);
+            let currSeq = 1;
+            
+            if(isStart) {
+                let nxt = num+1;
+                while(seenMap.has(nxt)) {
+                    seenMap.delete(nxt);
+                    currSeq++, nxt++
+                    // nxt++;
+                }
+
+                maxSeq = Math.max(currSeq, maxSeq)
+            };
+        }
+
+        
+        return maxSeq;
+    };
+};
+
