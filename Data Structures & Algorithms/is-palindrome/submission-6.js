0b3d392e996a42eddee7b4c61eb8e4d0ed6212f0
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-08-14 10:51:16 +0100

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,48 @@
+    const nonAlphaNum = {
+        "?": true,
+        "!": true,
+        ".": true,
+        ",": true,
+        ";": true,
+        ":": true,
+        "_": true,
+        "'": true,
+        " ": true,
+    }
+
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        if(s.length === 1) return true;
+
+        let l = 0;
+        let r = s.length-1;
+
+        s = s.toLowerCase();
+
+        while(l <= r) {
+            if(s[r] in nonAlphaNum) {
+                r--
+                continue;
+            }
+
+            if(s[l] in nonAlphaNum) {
+                l++
+                continue;
+            }
+
+            const isEqual = s[r] === s[l];
+            if(!isEqual) return false;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
+
+//    l            r
+// "No lemon, no melon"
