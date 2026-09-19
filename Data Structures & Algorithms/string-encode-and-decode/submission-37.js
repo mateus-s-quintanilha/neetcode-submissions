685c72a378584943363318bd89fe69ec378ff761
Mateus Quintanilha <100632445+mateus-s-quintanilha@users.noreply.github.com>
2026-09-19 17:57:35 +0100

    Add: string-encode-and-decode - submission-37

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-37.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    criptoKey = '%$#'
+    
+    encode(strs) {
+        
+        const encodedStr = strs.join(this.criptoKey);
+        if(!strs.length) return '-1';
+        return encodedStr;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        if(str === '-1') return [];
+
+        const decodedStr = str.split(this.criptoKey)
+        return decodedStr;
+    }
+}
